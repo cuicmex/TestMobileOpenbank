3083ea69acbc2ea76b67f45db4caa57e99ca2b2e
Humberto Cuitlahuac Trujano Chavez <ctrujano@yahoo.com>
2022-01-28 22:22:52 +0100

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Humberto C Trujano Chavez on 28/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
